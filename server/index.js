a70a729a8b1305982a1b950cc0c92bf6e528aa79
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-02-27 13:55:27 +0700

    Create project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+// app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+// })
+
+app.get('/api', (req, res) => {
+    res.json({ message: "Api page" })
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT)
+})

14a51a3770466d00251159991208584a2dfcbe72
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-02-29 14:26:29 +0700

    Create build

--- server/database.sql
+++ server/database.sql
@@ -5,7 +5,7 @@ USE ROBOTIC_SHOP;
 CREATE TABLE IF NOT EXISTS tbl_categories(
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    IMAGE VARCHAR(255)
+    image VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS tbl_products(
@@ -22,22 +22,11 @@ CREATE TABLE IF NOT EXISTS tbl_products(
 
 ALTER TABLE tbl_products ADD CONSTRAINT FK_PRODUCTCATEGORY FOREIGN KEY (categoryID) REFERENCES tbl_products(id);
 
-INSERT INTO tbl_categories(name) VALUES 
-("Batteries"), 
-("Charger"), 
-("Wiring"),
-("Cables"),
-("Connectors"),
-("Data communication"),
-("Input devices"),
-("Data storage"),
-("Electronic components"),
-("Adapters/Converters"),
-("LCD and display"),
-("Lighting component"),
-("Motors and Actuators"), 
-("Microcontrollers"), 
-("Sensors");
+INSERT INTO tbl_categories(name, image) VALUES 
+("Robot Parts", "https://images.pexels.com/photos/1084213/pexels-photo-1084213.jpeg")
+("Kits", "https://images.pexels.com/photos/4219862/pexels-photo-4219862.jpeg")
+("Consumer Robots", "https://images.pexels.com/photos/3616738/pexels-photo-3616738.jpeg"),
+("Cables", "https://images.pexels.com/photos/415043/pexels-photo-415043.jpeg?");
 
 INSERT INTO tbl_products(name, description, image, price, quantity, sold, totalRating, categoryID) VALUES 
 ("Battery Holder - 6 x AA", "Plastic holder use for standard AA battery\nNumber of cell: 6\nDouble sided\nSold individually", "https://images.pexels.com/photos/1476321/pexels-photo-1476321.jpeg", 25000, 140, 46, 4.3, 1);
